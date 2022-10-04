26bdf52d8d1b19b12e2e918881539dca214fb61d
Victor Yuri <victoryuritc@yahoo.com>
2022-10-04 16:34:07 -0300

    generate rand num, turn into chars and, then, return randomToken

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const validUser = {
+  email: 'email@email.com',
+  password: '123456',
+};
+
+const validateLogin = async (req, res, next) => {
+  if (req.body === validUser) {
+    next();
+  }
+};
+
+// Part of the solution can be found here
+// : https://thewebdev.info/2021/10/13/how-to-create-a-random-token-in-javascript/
+const generateRandomToken = () => Math.random().toString(36).substring(2);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -27,4 +42,11 @@ app.get('/talker', async (req, res) => {
 
   const talkers = JSON.parse(await fs.readFile(pathTalkers, 'utf-8'));
   res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.post('/login', (req, res) => {
+  const randomToken = (generateRandomToken() + generateRandomToken()).substring(1, 17);
+  res.status(HTTP_OK_STATUS).json({
+    token: randomToken,
+  });
 });
